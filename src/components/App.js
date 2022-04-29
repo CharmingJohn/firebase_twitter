16e6ff74b7b5bfadf9fd0fb9d262d0ae190214e4
CharmingJohn <chaechunjae@gmail.com>
2022-04-29 23:05:20 +0900

    good

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
-import AppRouter from "./Router";
+import React from 'react'
+import AppRouter from "components/Router";
+import { useEffect, useState } from "react";
+import { authService } from "fbase";
 
 function App() {
-  return <AppRouter />;
+  const [ init, setInit] = useState(false);
+  const [ isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+  setInterval(() => console.log(authService.currentUser), 2000);
+  console.log(authService.currentUser)
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn= { isLoggedIn }/> : "initializing..."}
+      <footer>&copy; {new Date().getFullYear()} firebase_twitter </footer>
+    </>
+  );
 }
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,10 @@
+import React from 'react'
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
 
-const AppRouter = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({ isLoggedIn }) => {
+
     return (
         <Router>
             <Routes>
