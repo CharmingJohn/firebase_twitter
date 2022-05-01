d7d1469e009b1f9f3fc195ed6898083099035db8
CharmingJohn <45421181+CharmingJohn@users.noreply.github.com>
2022-05-01 22:36:20 +0900

    you can do it

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,28 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom"
 
-const Profile = () => {
+const Profile = ( {userObj} ) => {
 
     const navigate = useNavigate();
 
     const onLogOutClik = () =>{
         authService.signOut();
-        navigate("/");    
+        navigate("/");
+    };
+    /*
+    const getMyFtweets = async () => {
+        const ftweets = await dbService
+        .collection("ftweets")
+        .where("creatorId", "==", userObj.uid)
+        .orderBy("createdAy", "asc")
+        .get();
     };
 
+    useEffect(() => {
+        getMyFtweets();
+    }, []);
+    */
     return (
     <>
         <button onClick={onLogOutClik}>Log Out</button>
